219d01af8857dfd478adaf0a8e5b7cf2c4729bac
Luan Campos <luancamposdeveloper@gmail.com>
2026-02-08 14:42:50 -0300

    Add NameVO with validation logic and test coverage

---
+++ src/core/users/value-objects/name.vo.ts
@@ -0,0 +1,30 @@
+export class NameVO {
+	private readonly _name: string;
+
+	public get value(): string {
+		return this._name;
+	}
+
+	static create(name: string): NameVO {
+		if (!NameVO.isValid(name)) {
+			throw new Error("Invalid name, Please enter valid name.");
+		}
+
+		return new NameVO(name.trim());
+	}
+
+	private static isValid(name: string): boolean {
+		if (!name) return false;
+
+		const trimmed: string = name.trim();
+		const parts: string[] = trimmed.split(/\s+/);
+
+		if (parts.length !== 2) return false;
+
+		return parts.every((part) => /^[A-Za-zÀ-ÿ]{2,}$/.test(part));
+	}
+
+	constructor(name: string) {
+		this._name = name;
+	}
+}

efe347c9f1c2e633523658b48e4c2dcd121515a4
Luan Campos <luancamposdeveloper@gmail.com>
2026-02-12 00:03:02 -0300

    Uncomment methods in UsersRepository for full interface implementation

--- src/core/users/users.repository.ts
+++ src/core/users/users.repository.ts
@@ -4,12 +4,12 @@ import type {EmailVO} from "@core/users/value-objects/email.vo";
 
 export abstract class UsersRepository {
   abstract findById(id: string): Promise<UsersEntity | null>;
-  // abstract findByEmail(email: EmailVO): Promise<UsersEntity | null>;
+  abstract findByEmail(email: EmailVO): Promise<UsersEntity | null>;
   abstract register(user: UsersEntity): Promise<void>;
-  // abstract save(user: UsersEntity): Promise<void>;
+  abstract save(user: UsersEntity): Promise<void>;
   abstract existsByEmail(email: EmailVO): Promise<boolean>;
-  // abstract findBySocialLogin(
-  //   provider: string,
-  //   providerId: string,
-  // ): Promise<UsersEntity | null>;
+  abstract findBySocialLogin(
+    provider: string,
+    providerId: string,
+  ): Promise<UsersEntity | null>;
 }
